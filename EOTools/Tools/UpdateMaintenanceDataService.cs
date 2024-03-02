93dc6088d921241a00f547ec899101d0af78c7a0
Jebzou <22751386+Jebzou@users.noreply.github.com>
2024-03-02 08:55:17 +0100

    New maint logic

--- EOTools/Tools/UpdateMaintenanceDataService.cs
+++ EOTools/Tools/UpdateMaintenanceDataService.cs
@@ -32,25 +32,8 @@ public class UpdateMaintenanceDataService
 
         JObject updateData = JsonHelper.ReadJsonObject(UpdateFilePath);
 
-        // get last update : 
-        (UpdateModel? update, int updState) = GetMaintenanceState();
-
-        if (update is { UpdateDate: { } updateDate, UpdateStartTime: { } updateTime })
-        {
-            updateData["kancolle_mt"] = $"{updateDate.Date.Add(updateTime):yyyy/MM/dd HH:mm:ss}";
-
-            updateData["MaintInfoLink"] = string.IsNullOrEmpty(update.EndTweetLink) switch
-            {
-                true => string.IsNullOrEmpty(update.StartTweetLink) switch
-                {
-                    true => "https://twitter.com/KanColle_STAFF",
-                    false => update.StartTweetLink
-                },
-                false => update.EndTweetLink,
-            };
-        }
-
-        updateData["event_state"] = updState;
+        SetOldUpdateTime(updateData);
+        UpdateModel? update = SetUpdateTime(updateData);
 
         new DatabaseSyncService().StageDatabaseChangesToGit();
 
@@ -60,14 +43,96 @@ public class UpdateMaintenanceDataService
 
         string commitMessage = update switch
         {
-            UpdateModel => $"Maintenance information - {update.Name}",
-            _ => "Clear maintenance information"
+            not null => $"Maintenance information - {update.Name}",
+            _ => "Clear maintenance information",
         };
 
         GitManager.CommitAndPush(commitMessage);
     }
 
+    private UpdateModel? SetUpdateTime(JObject updateData)
+    {
+        // get last update : 
+        (UpdateModel? update, int updState) = GetMaintenanceState();
+
+        if (update is { UpdateDate: { } updateDate, UpdateStartTime: { } updateStartTime })
+        {
+            updateData["MaintStart"] = $"{updateDate.Date.Add(updateStartTime):yyyy/MM/dd HH:mm:ss}";
+            updateData["MaintEnd"] = update.UpdateEndTime switch
+            {
+                {} endTime => $"{updateDate.Date.Add(endTime):yyyy/MM/dd HH:mm:ss}",
+                _ => null,
+            };
+
+            updateData["MaintInfoLink"] = string.IsNullOrEmpty(update.EndTweetLink) switch
+            {
+                true => string.IsNullOrEmpty(update.StartTweetLink) switch
+                {
+                    true => "https://twitter.com/KanColle_STAFF",
+                    false => update.StartTweetLink,
+                },
+                false => update.EndTweetLink,
+            };
+        }
+
+        updateData["MaintEventState"] = updState;
+
+        return update;
+    }
+
+    private void SetOldUpdateTime(JObject updateData)
+    {
+        // get last update : 
+        (UpdateModel update, int updState) = GetOldMaintenanceState();
+
+        if (update is { UpdateDate: { } updateDate, UpdateStartTime: { } updateTime })
+        {
+            updateData["kancolle_mt"] = $"{updateDate.Date.Add(updateTime):yyyy/MM/dd HH:mm:ss}"; // For backward compatibility
+        }
+
+        updateData["event_state"] = updState;
+    }
+
     private (UpdateModel?, int) GetMaintenanceState()
+    {
+        // get last update : 
+        using EOToolsDbContext db = new();
+
+        UpdateModel? update = db.Updates
+            .AsEnumerable()
+            .Where(upd => !upd.WasLiveUpdate)
+            .Where(upd => upd.UpdateDate is not null)
+            .Where(upd => upd.UpdateStartTime is not null)
+            .Where(upd => upd.UpdateInProgress() || upd.UpdateIsComing())
+            .MinBy(upd => upd.UpdateDate);
+
+        if (update is null)
+        {
+            return (null, (int)MaintenanceState.None);
+        }
+
+        EventModel? eventEnd = db.Events
+            .AsEnumerable()
+            .FirstOrDefault(ev => ev.EndOnUpdateId == update.Id);
+
+        if (eventEnd != null)
+        {
+            return new(update, (int)MaintenanceState.EventEnd);
+        }
+
+        EventModel? eventStart = db.Events
+            .AsEnumerable()
+            .FirstOrDefault(ev => ev.StartOnUpdateId == update.Id);
+
+        if (eventStart != null)
+        {
+            return new(update, (int)MaintenanceState.EventStart);
+        }
+
+        return new(update, (int)MaintenanceState.Regular);
+    }
+
+    private (UpdateModel?, int) GetOldMaintenanceState()
     {
         // get last update : 
         using EOToolsDbContext db = new();
